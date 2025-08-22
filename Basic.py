c4f5e43e14b409a5ef11dac6b2bd46e1e2a72169
Kevil <kevilrathod311@gmail.com>
2025-08-23 00:47:01 +0530

    Create Basic.py

---
+++ Basic.py
@@ -0,0 +1,11 @@
+#WAP to ask the user to enter the names of three movies and store them in a list.
+
+movies = []
+mov1 = input("Enter movie 1")
+movies.append(mov1)
+mov2 = input("Enter movie 2")
+movies.append(mov2)
+mov3 = input("Enter movie 3")
+movies.append(mov3)
+
+print(movies)
